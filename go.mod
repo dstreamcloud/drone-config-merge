c6f2209edf37fb639d15507178c3dcc2c41864ca
Qiaosen Huang <joesonw@gmail.com>
2020-04-30 14:30:49 +0800

    use github app auth

--- go.mod
+++ go.mod
@@ -3,15 +3,17 @@ module github.com/dstreamcloud/drone-config-merge
 go 1.13
 
 require (
-	github.com/drone/drone-go v1.3.1 // indirect
-	github.com/go-git/go-git v4.7.0+incompatible // indirect
-	github.com/go-git/go-git/v5 v5.0.0 // indirect
-	github.com/google/go-github v17.0.0+incompatible // indirect
-	github.com/kelseyhightower/envconfig v1.4.0 // indirect
-	github.com/sirupsen/logrus v1.5.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/drone/drone-go v1.3.1
+	github.com/google/go-cmp v0.3.0 // indirect
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/sirupsen/logrus v1.5.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -3,24 +3,32 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"path/filepath"
 	"strings"
+	"sync"
+	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/config"
 	"github.com/google/go-github/github"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/oauth2"
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	Secret      string `envconfig:"DRONE_YAML_SECRET"`
-	Addr        string `envconfig:"DRONE_PLUGIN_ADDR"`
-	GithubToken string `envconfig:"DRONE_PLUGIN_GITHUB_TOKEN"`
+	Secret                  string `envconfig:"DRONE_YAML_SECRET"`
+	Addr                    string `envconfig:"DRONE_PLUGIN_ADDR"`
+	GithubAPPID             string `envconfig:"DRONE_PLUGIN_GITHUB_APP_ID"`
+	GithubAPPInstallationID string `envconfig:"DRONE_PLUGIN_GITHUB_APP_INSTALLATION_ID"`
+	GithubAPPPrivateKey     string `envconfig:"DRONE_PLUGIN_GITHUB_APP_PRIVATE_KEY"`
 }
 
 type plugin struct {
@@ -93,15 +101,103 @@ func (p *plugin) Find(ctx context.Context, req *config.Request) (*drone.Config, 
 	}, nil
 }
 
+type githubAppAuthenticator struct {
+	id                 string
+	installationID     string
+	privateKey         []byte
+	accessToken        string
+	accessTokenExpires time.Time
+	accessTokenOnce    *sync.Once
+	accessTokenError   error
+}
+
+func (a *githubAppAuthenticator) RoundTrip(req *http.Request) (*http.Response, error) {
+	if a.accessTokenExpires.Before(time.Now().Add(time.Second * 10)) {
+		a.accessTokenOnce.Do(a.getAccessToken)
+		if a.accessTokenError != nil {
+			return nil, a.accessTokenError
+		}
+	}
+
+	req.Header.Set("Authorization", "token "+a.accessToken)
+	return http.DefaultClient.Do(req)
+}
+
+func (a *githubAppAuthenticator) getAccessToken() {
+	a.accessTokenError = nil
+	tok := jwt.NewWithClaims(jwt.SigningMethodRS256, &jwt.StandardClaims{Issuer: a.id, IssuedAt: time.Now().Unix(), ExpiresAt: time.Now().Add(time.Second * 10).Unix()})
+	tokString, err := tok.SignedString(a.privateKey)
+	if err != nil {
+		a.accessTokenError = err
+		return
+	}
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.github.com/app/installations/%s/access_tokens", a.installationID), nil)
+	if err != nil {
+		a.accessTokenError = err
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+tokString)
+	req.Header.Set("Accept", "application/vnd.github.machine-man-preview+json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		a.accessTokenError = err
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		a.accessTokenError = err
+		return
+	}
+
+	if res.StatusCode%100 != 2 {
+		a.accessTokenError = errors.New(string(body))
+		return
+	}
+
+	result := map[string]interface{}{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		a.accessTokenError = err
+		return
+	}
+
+	accessToken, ok := result["token"].(string)
+	if !ok {
+		a.accessTokenError = errors.New(string(body))
+		return
+	}
+
+	expire, ok := result["expires_at"].(string)
+	if !ok {
+		a.accessTokenError = errors.New(string(body))
+		return
+	}
+	expireAt, err := time.Parse("2006-01-02T15:04:05Z", expire)
+	if err != nil {
+		a.accessTokenError = err
+		return
+	}
+	a.accessToken = accessToken
+	a.accessTokenExpires = expireAt
+}
+
 func main() {
 	cfg := &Config{}
 	envconfig.MustProcess("", cfg)
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: cfg.GithubToken},
-	)
-	tc := oauth2.NewClient(context.Background(), ts)
+	client := &http.Client{
+		Transport: &githubAppAuthenticator{
+			id:              cfg.GithubAPPID,
+			installationID:  cfg.GithubAPPInstallationID,
+			privateKey:      []byte(cfg.GithubAPPPrivateKey),
+			accessTokenOnce: &sync.Once{},
+		},
+	}
+
 	p := &plugin{
-		client: github.NewClient(tc),
+		client: github.NewClient(client),
 	}
 	handler := config.Handler(p, cfg.Secret, logrus.StandardLogger())
 	http.Handle("/", handler)
