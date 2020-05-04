f9a90c27d6132a57f5e493aba41c3dcbd3487fe0
Qiaosen Huang <joesonw@gmail.com>
2020-05-05 00:21:55 +0800

    allow template pipeline

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -3,16 +3,16 @@ package plugin
 import (
 	"bytes"
 	"context"
-	"encoding/json"
+	"fmt"
 	"io"
 	"path/filepath"
 	"strings"
+	"text/template"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/config"
 	"github.com/google/go-github/github"
 	"github.com/sirupsen/logrus"
-	"github.com/tidwall/gjson"
 	"gopkg.in/yaml.v2"
 )
 
@@ -35,8 +35,78 @@ func (p *Plugin) Find(ctx context.Context, req *config.Request) (*drone.Config, 
 	}
 	decoder := yaml.NewDecoder(strings.NewReader(entryBody))
 	var records []map[string]interface{}
-	var dependsOn []string
 	var statuses []*github.RepoStatus
+	templates := map[string]*template.Template{}
+
+	var handleRecord func(record map[string]interface{}) error
+	handleRecord = func(record map[string]interface{}) error {
+		switch record["kind"] {
+		case "template-pipeline":
+			{
+				str := record["template"].(string)
+				name := record["name"].(string)
+				tpl, err := template.New(str).Parse(str)
+				if err != nil {
+					return err
+				}
+				templates[name] = tpl
+				return nil
+			}
+		case "from-pipeline-template":
+			{
+				name := record["template"].(string)
+				tpl, ok := templates[name]
+				if !ok {
+					return fmt.Errorf("unable to find template: " + name)
+				}
+				buf := bytes.NewBuffer(nil)
+				if err := tpl.Execute(buf, record["variables"]); err != nil {
+					return err
+				}
+				templateRecord := map[string]interface{}{}
+				if err := yaml.Unmarshal(buf.Bytes(), &templateRecord); err != nil {
+					return err
+				}
+				records = append(records, templateRecord)
+				return nil
+			}
+		case "virtual-pipeline":
+			{
+				pipelines := record["pipelines"].([]interface{})
+				for _, item := range pipelines {
+					key := item.(string)
+					droneYAML := filepath.Join(key, req.Repo.Config)
+					// TODO parallelism of fetching drone.yml
+					content, _, _, err := p.client.Repositories.GetContents(ctx, req.Repo.Namespace, req.Repo.Name, droneYAML, &github.RepositoryContentGetOptions{Ref: req.Build.After})
+					if err != nil {
+						return err
+					}
+
+					body, err := content.GetContent()
+					if err != nil {
+						return err
+					}
+					childRecord := map[string]interface{}{}
+					if err := yaml.Unmarshal([]byte(body), &childRecord); err != nil {
+						return err
+					}
+					statuses = append(statuses, &github.RepoStatus{
+						TargetURL: github.String(req.Repo.HTTPURL + "/blob/" + req.Build.After + "/" + droneYAML),
+						State:     github.String("success"),
+						Context:   github.String("config-merge/" + droneYAML),
+					})
+					if err := handleRecord(childRecord); err != nil {
+						return err
+					}
+				}
+				return nil
+			}
+		default:
+			records = append(records, record)
+		}
+		return nil
+	}
+
 	for {
 		record := map[string]interface{}{}
 		if err := decoder.Decode(&record); err != nil {
@@ -45,51 +115,14 @@ func (p *Plugin) Find(ctx context.Context, req *config.Request) (*drone.Config, 
 			}
 			return nil, err
 		}
-
-		if record["kind"] == "virtual-pipeline" {
-			recordBytes, _ := json.Marshal(&record)
-			recordStr := string(recordBytes)
-			pipelines := gjson.Get(recordStr, "pipelines")
-
-			for _, item := range pipelines.Array() {
-				key := item.String()
-
-				droneYAML := filepath.Join(key, req.Repo.Config)
-				// TODO parallelism of fetching drone.yml
-				content, _, _, err := p.client.Repositories.GetContents(ctx, req.Repo.Namespace, req.Repo.Name, droneYAML, &github.RepositoryContentGetOptions{Ref: req.Build.After})
-				if err != nil {
-					return nil, err
-				}
-
-				body, err := content.GetContent()
-				if err != nil {
-					return nil, err
-				}
-				record := map[string]interface{}{}
-				if err := yaml.Unmarshal([]byte(body), &record); err != nil {
-					return nil, err
-				}
-				recordBytes, _ := json.Marshal(&record)
-				recordStr := string(recordBytes)
-				dependsOn = append(dependsOn, gjson.Get(recordStr, "name").String())
-				statuses = append(statuses, &github.RepoStatus{
-					TargetURL: github.String(req.Repo.HTTPURL + "/blob/" + req.Build.After + "/" + droneYAML),
-					State:     github.String("success"),
-					Context:   github.String("config-merge/" + droneYAML),
-				})
-				records = append(records, record)
-			}
-			continue
+		if err := handleRecord(record); err != nil {
+			return nil, err
 		}
-		records = append(records, record)
 	}
 
 	output := bytes.NewBuffer(nil)
 	encoder := yaml.NewEncoder(output)
 	for _, record := range records {
-		if record["injectDependencies"] == true {
-			record["depends_on"] = dependsOn
-		}
 		if err := encoder.Encode(&record); err != nil {
 			return nil, err
 		}
